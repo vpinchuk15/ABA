8e8bc8dad4607e8551b4ab6bbc6baec79b6ade5c
Vladimir Pinchuk <vpinchuk@sandiego.edu>
2021-04-15 22:11:25 -0700

    Logging In adjusted

--- authentication.py
+++ authentication.py
@@ -3,27 +3,48 @@
 #Date: Updated: April 15, 2021
 #Login file is called = Login.txt
 
-def login(actualUser,actualPass):
+def login(actualUser):
     """
-    Code retrieved:
+    Part of the code retrieved:
     https://stackoverflow.com/questions/46747524/creating-a-login-program-that-recalls-information-from-text-files
     """
     logged_in = False
-    with open('Login.txt', 'r') as file:
+    
+    with open('Login.txt', 'w+') as file:
         for line in file:
-            username, password = line.split(',')
-            # Check the username against the one supplied
+            #TODO: Add status to see if already logged it
+            username, password, status = line.split(',')
+            #Check the username against the one supplied
             if username == actualUser:
+                #Case 4
+                if (actualPass == ""):
+                    newPassword = input("This is the first time the account is being used. You must create a new password.\nPasswords may contain 1-24 upper- or lower-case letters or numbers. Choose an uncommon password that would be difficult to guess.")
+                    if (len(newPassword) < 25 and len(newPassword) > 0 and newPassword.isalnum() and (newPassword.isnumeric() or newPassword.isalpha())):
+                        return ("Password is too easy to guess")
+                    elif (len(newPassword) < 25 and len(newPassword) > 0 and newPassword.isalnum()):
+                        checkNew = input("Reenter the same password: ")
+                        if (checkNew == newPassword):
+                            file.write(actualUser + "," + newPassword + ",1" +'\n')
+                            return ("Ok")
+                        else:
+                            return ("Passwords do not match")
+                    else:
+                        return ("Password contains illegal characters")
+                actualPass = input("Enter your password: ")
                 logged_in = password == actualPass
-                break
-
-    if logged_in:
-        return True
-    else:
-        return False
+                if logged_in:
+                    if status == "1":
+                        #Case 1
+                        return ("An account is currently active; logout before proceeding.")
+                    else:
+                        #Case 3
+                        return True
+                else:
+                    #Case 2
+                    return ("Invalid credentials.")
 
 def logout():
-    #TODO: Logout and possibly save in logs?
+    #TODO: Logout and possibly save in logs? Put 0 status
     pass
 
 def changePassword():
