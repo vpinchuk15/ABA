75487dc181e9879945135cf18987e5a79d0bb943
Nathan Shah <nathanshah@sandiego.edu>
2021-04-16 14:42:52 -0700

    Import and export left

--- datacom.py
+++ datacom.py
@@ -48,7 +48,7 @@ def addRecord(username, recordID, SN = None, GN = None, PEM = None, WEM = None, 
     """
     Adds a record to the given users contacts.
     """
-    if not addRecordCheck(recordID, SN, GN, PEM, WEM, PPH, WPH, SA, CITY, PC):
+    if not check(recordID, SN, GN, PEM, WEM, PPH, WPH, SA, CITY, PC):
         return None
     else:
         users = openDatabase()
@@ -71,7 +71,7 @@ def addRecord(username, recordID, SN = None, GN = None, PEM = None, WEM = None, 
         print("OK")
         saveDatabase(users)
 
-def addRecordCheck(recordID, SN = None, GN = None, PEM = None, WEM = None, PPH = None,
+def check(recordID, SN = None, GN = None, PEM = None, WEM = None, PPH = None,
                 WPH = None, SA = None, CITY = None, PC = None):
     """
     Validates Inputs to addRecord method
@@ -101,15 +101,145 @@ def deleteRecord(username, recordID):
     Deletes a record from the given users contacts.
     """
 
-def editRecord(username, recordID):
+    users = openDatabase()
+
+    if recordID == '':
+        print("No recordID")
+        return None
+    
+    if len(recordID) > 64:
+        print("Invalid recordID")
+        return None
+
+    if users[username].contacts.get(recordID, None) == None:
+        print("RecordID not found")
+        return None
+    
+    users[username].contacts.pop(recordID)
+
+    print("OK")
+
+    saveDatabase(users)
+
+    return None
+    
+def editRecord(username, recordID, SN = None, GN = None, PEM = None, WEM = None, PPH = None,
+                WPH = None, SA = None, CITY = None, PC = None):
     """
     Edits a record from the given users contacts.
     """
+
+    if not check(recordID, SN, GN, PEM, WEM, PPH, WPH, SA, CITY, PC):
+        return None
+    else:
+        users = openDatabase()
+
+        if users[username].contacts.get(recordID, None) == None:
+            print("RecordID not found")
+            return None
+
+        if SN != None:
+            users[username].contacts[recordID].SN = SN
+        if GN != None:
+            users[username].contacts[recordID].GN = GN
+        if PEM != None:
+            users[username].contacts[recordID].PEM = PEM
+        if WEM != None:
+            users[username].contacts[recordID].WEM = WEM
+        if PPH != None:
+            users[username].contacts[recordID].PPH = PPH
+        if WPH != None:
+            users[username].contacts[recordID].WPH = WPH
+        if SA != None:
+            users[username].contacts[recordID].SA = SA
+        if CITY != None:
+            users[username].contacts[recordID].CITY = CITY
+        if PC != None:
+            users[username].contacts[recordID].PC = PC
+
+        print("OK")
+        
+        saveDatabase(users)
+
+    return None
+
 def readRecord(username, recordID, SN = None, GN = None, PEM = None, WEM = None, PPH = None,
                 WPH = None, SA = None, CITY = None, PC = None):
     """
     Prints the requested information to the user about th record of interest.
     """
+    if recordID == '':
+        print("No recordID")
+        return None
+    
+    if len(recordID) > 64:
+        print("Invalid recordID")
+        return None
+
+    fieldValues = [SN, GN, PEM, WEM, PPH, WPH, SA, CITY, PC]
+    accecptedFieldValues = ['SN', 'GN', 'PEM', 'WEM', 'PPH', 'WPH', 'SA', 'CITY', 'PC', None]
+
+    for value in fieldValues:
+        if value not in accecptedFieldValues:
+            print("Invalid fieldname(s)")
+            return None
+    
+    users = openDatabase()
+
+    if users[username].contacts.get(recordID, None) == None:
+        print("RecordID not found")
+        return None
+                
+    output = recordID
+
+    total = 0
+    for value in fieldValues:
+        if value == None:
+            total += 1
+    
+    if total == len(fieldValues):
+        output = (output + ' SN=' + exist(users[username].contacts[recordID].SN) + ' GN=' + exist(users[username].contacts[recordID].GN)
+                        + ' PEM=' + exist(users[username].contacts[recordID].PEM) + ' WEM=' + exist(users[username].contacts[recordID].WEM)
+                        + ' PPH=' + exist(users[username].contacts[recordID].PPH) + ' WPH=' + exist(users[username].contacts[recordID].WPH)
+                        + ' SA=' + exist(users[username].contacts[recordID].SA) + ' CITY=' + exist(users[username].contacts[recordID].CITY)
+                        + ' PC=' + exist(users[username].contacts[recordID].PC) )
+
+    else:
+        for value in fieldValues:
+            if value == None:
+                continue   
+            if value == 'SN':
+                output = output + ' SN=' + users[username].contacts[recordID].SN
+            if value == 'GN':
+                output = output + ' GN=' + users[username].contacts[recordID].GN
+            if value == 'PEM':
+                output = output + ' PEM=' + users[username].contacts[recordID].PEM
+            if value == 'WEM':
+                output = output + ' WEM=' + users[username].contacts[recordID].WEM
+            if value == 'PPH':
+                output = output + ' PPH=' + users[username].contacts[recordID].PPH
+            if value == 'WPH':
+                output = output + ' WPH=' + users[username].contacts[recordID].WPH
+            if value == 'SA':
+                output = output + ' SA=' + users[username].contacts[recordID].SA
+            if value == 'CITY':
+                output = output + ' CITY=' + users[username].contacts[recordID].CITY
+            if value == 'PC':
+                output = output + ' PC=' + users[username].contacts[recordID].PC
+
+    print(output)
+    print("OK")
+
+    return None
+
+def exist(value):
+    """
+    Helpful function for readRecord
+    """
+    if value == None:
+        return ''
+    else:
+        return value
 
 def importDatabase(username, filename):
     """
