a1d1f40e0567b6465c4768d901b1bf198683f348
Nathan Shah <nathanshah@sandiego.edu>
2021-04-19 12:21:54 -0700

    Edits work together

---
+++ hubEdit.py
@@ -0,0 +1,245 @@
+#ABA Module: HUB
+#Authors: Dominic Santilla, Nathan Shah
+#Date: Updated: April 17, 2021
+
+import datacomEdit as datacom
+import loginEdit as login
+import auditLogEdit as auditLog
+
+class Session:
+
+    def __init__(self):
+        self.username = None
+        self.access = 0
+
+def runSession():
+
+    print("Address Book Application, version "+ '0.1' + ". Type “HLP” for a list of commands.")
+    
+    session = Session()
+
+    datacom.UserDatabase()
+    auditLog.AuditLog()
+    login.Login()
+    
+    while(True):
+        print()
+        command_str = input("Enter Command>")
+
+        command = command_str[:3]
+
+        fieldValues = command_str[3:].strip()
+        
+        if command == 'EXT':
+                session.username = None
+                session.access = 0
+                print("OK")
+                exit()
+
+        elif session.username == None:  
+            
+            if command == "LIN":
+                code,audit = login.login(fieldValues)
+
+                if code:
+                    session.username = fieldValues
+                    auditLog.addLog(audit,session.username)
+
+                    if session.username == 'admin':
+                        session.access = 1
+                    else:
+                        session.access = 0
+
+                else:
+                    if audit != '':
+                        auditLog.addLog(audit,fieldValues)
+                        print("Access NOT granted")
+
+            elif command == "HLP":
+                print("Login: LIN userID")
+                print("Exit = EXT")
+                print("Logout = LOU")
+                print("Change Password = CHP userID")
+                print("Add User (Admin Only) = ADU userID")
+                print("Delete User (Admin Only) = DEU userID")
+                print("Display Audit Log (Admin Only) = DAL")
+                print("List Users (Admin Only) = LSU")
+                print("Add Record = ADR recordID fieldvalues")
+                print("Delete Record = DER recordID fieldvalues")
+                print("Edit Record = EDR recordID fieldvalues")
+                print("Read Record = RER recordID fieldvalues")
+                print("Import Database = IMD filename")
+                print("Export Database = EXD filename")
+
+            else:
+                print("Need to login to access other commands or command is not valid.")
+
+        elif command == "LIN":
+            print("Already logged in.")
+
+        elif command == "LOU":
+
+            auditLog.addLog('LO', session.username)
+            session.username = None
+            session.access = 0
+            
+        elif command == "CHP":
+
+            code,audit = login.changePassword(fieldValues)
+
+            if code:
+                auditLog.addLog(audit,session.username)
+
+            else:
+                if audit != '':
+                    auditLog.addLog(audit,fieldValues)
+
+            
+        elif session.access == 1:
+
+            if command == "ADU":
+                login.addUser(fieldValues)
+                auditLog.addLog("AU", 'admin')
+
+            elif command == "DEU":
+                login.deleteUser(fieldValues)
+                auditLog.addLog("DU", 'admin')
+
+            elif command == "DAL":
+                if(len(fieldValues) > 0):
+                    auditLog.displayLog(fieldValues)
+                else:
+                    auditLog.displayLog()
+
+            elif command == "LSU":
+                login.listUsers()
+            
+            else:
+                print("Command is not valid.")
+        
+        else:
+            if command == "ADR":
+                fv = parse(fieldValues)
+                datacom.addRecord(session.username, fv[0], fv[1], fv[2], fv[3], fv[4], fv[5], fv[6], fv[7], fv[8],
+                                   fv[9], fv[10], fv[11] )
+                continue
+
+            elif command == "DER":
+                fv = parse2(fieldValues)
+                datacom.deleteRecord(session.username,fv)
+                continue
+
+            elif command == "EDR":
+                fv = parse(fieldValues)
+                datacom.editRecord(session.username, fv[0], fv[1], fv[2], fv[3], fv[4], fv[5], fv[6], fv[7], fv[8],
+                                    fv[9], fv[10], fv[11] )
+                continue
+
+            elif command == "RER":
+                fv = parse3(fieldValues)
+                datacom.readRecord(session.username, fv[0], fv[1], fv[2], fv[3], fv[4], fv[5], fv[6], fv[7], fv[8],
+                                   fv[9], fv[10], fv[11] )
+                continue
+
+            elif command == "IMD":
+                fv = parse2(fieldValues)
+                datacom.importDatabase(session.username, fv)
+                continue
+
+            elif command == "EXD":
+                try:
+                    fv = parse2(fieldValues)
+                    datacom.exportDatabase(session.username,fv)
+                except:
+                    print("No data to export.")
+                continue
+
+            else:
+                print("Command is not valid.")
+
+def parse(fieldValues):
+    """
+    Creates values for entry into ADR, EDR
+    """
+    cleaned = ['']*12
+
+    if len(fieldValues) == 0:
+        return cleaned
+    else:
+        output = [s for s in fieldValues.split('"') if s.strip() != '']
+        cleaned[0] = output[0].strip()
+
+    for i in range(1,len(output),2):
+        if output[i] == 'SN=':
+            cleaned[1] = output[i+1]
+        elif output[i] == 'GN=':
+            cleaned[2] = output[i+1]
+        elif output[i] == 'PEM=':
+            cleaned[3] = output[i+1]
+        elif output[i] == 'WEM=':
+            cleaned[4] = output[i+1]
+        elif output[i] == 'PPH=':
+            cleaned[5] = output[i+1]
+        elif output[i] == 'WPH=':
+            cleaned[6] = output[i+1]
+        elif output[i] == 'SA=':
+            cleaned[7] = output[i+1]
+        elif output[i] == 'CITY=':
+            cleaned[8] = output[i+1]
+        elif output[i] == 'STP=':
+            cleaned[9] = output[i+1]
+        elif output[i] == 'CTY=':
+            cleaned[10] = output[i+1]
+        elif output[i] == 'PC=':
+            cleaned[11] = output[i+1]
+        else:
+            continue
+    
+    return cleaned
+
+def parse2(fieldValues):
+    output = fieldValues.split()
+
+    return output[0]
+
+def parse3(fieldValues):
+
+    cleaned = [None]*12
+
+    if len(fieldValues) == 0:
+        return cleaned
+    else:
+        output = [s for s in fieldValues.split()]
+        cleaned[0] = output[0].lstrip()
+
+    for i in range(1,len(output),2):
+        if output[i] == 'SN':
+            cleaned[1] = output[i]
+        elif output[i] == 'GN':
+            cleaned[2] = output[i]
+        elif output[i] == 'PEM':
+            cleaned[3] = output[i]
+        elif output[i] == 'WEM':
+            cleaned[4] = output[i]
+        elif output[i] == 'PPH':
+            cleaned[5] = output[i]
+        elif output[i] == 'WPH':
+            cleaned[6] = output[i]
+        elif output[i] == 'SA':
+            cleaned[7] = output[i]
+        elif output[i] == 'CITY':
+            cleaned[8] = output[i]
+        elif output[i] == 'STP':
+            cleaned[9] = output[i]
+        elif output[i] == 'CTY':
+            cleaned[10] = output[i]
+        elif output[i] == 'PC':
+            cleaned[11] = output[i]
+        else:
+            continue
+
+    return cleaned
+
+if __name__ == "__main__":
+    runSession()
+
---
+++ loginEdit.py
@@ -0,0 +1,183 @@
+#ABA Module: Login
+#Authors: Nathan Shah
+#Date: Updated: April 17, 2021
+
+import pickle
+import bcrypt #noqa
+
+
+class Login:
+    """
+    Models the user database.
+    """
+    def __init__(self):
+        self.table = {}
+        
+        self.table['admin'] = None
+
+        try:
+            open("loginFile.txt", "x")
+            with open("loginFile.txt", "wb") as f:
+                pickle.dump(self.table, f)
+
+            f.close()
+        except FileExistsError:
+            return None
+
+def login(userID):
+    """
+    Logins a user
+    """
+    table = openTable()
+
+    if userID not in table:
+        print("Invalid Credentials")
+        return False,''
+
+    if table[userID] == None:
+        print("This is the first time the account is being used. You must create a new password. Passwords may contain 1-24 upper- or lower-case letters or numbers. Choose an uncommon password that would be difficult to guess.")
+
+        password = input("Enter Password:")
+        password1 = input("Renter Password:")
+
+        if password != password1:
+            print("Passwords do not match.")
+            return False, 'LF'
+        
+        if not password.isascii():
+            print("Password contains illegal characters.")
+            return False, 'LF'
+
+        if password.isalpha() or password.isdigit() or password.isalnum():
+            print("Password is too easy to guess.")
+            return False, 'LF'
+
+        table[userID] = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        saveTable(table)
+
+        return True, 'LS, L1'
+
+    else:
+        password = input("Enter password:")
+        
+        if bcrypt.checkpw(password.encode("utf-8"), table[userID]):
+            print("OK")
+            return True, 'LS'
+        else:
+            return False, 'LF'
+
+def addUser(userID):
+    """
+    Admin only - add user
+    """
+    table = openTable()
+
+    if len(userID) > 64:
+        print("Invalid UserID")
+        return False, ''
+
+    if len(table) > 8:
+        print("Too many accounts.")
+        return False, ''
+
+    if table.get(userID, None) != None:
+        print("Account already exists")
+        return False, ''
+    
+    table[userID] = None
+
+    saveTable(table)
+
+    return True, 'AD'
+
+def deleteUser(userID):
+    """
+    Admin only - delete user
+    """
+    table = openTable()
+
+    if len(userID) > 64:
+        print("Invalid UserID")
+        return False, ''
+
+    if table.get(userID, None) == None:
+        print("Account does not exists")
+        return False, ''
+
+    table.pop(userID)
+
+    saveTable(table)
+
+    print("OK")
+    return True, 'DA'
+
+
+def changePassword(userID):
+    """
+    Changes Password
+    """
+    table = openTable()
+
+    oldPassword = input("Enter old password:")
+    
+    if bcrypt.checkpw(oldPassword.encode("utf-8"), table[userID]):
+        
+        password = input("Enter New Password:")
+        password1 = input("Renter New Password:")
+
+        if password != password1:
+            print("Passwords do not match.")
+            return False, 'FPC'
+        
+        if not password.isascii():
+            print("Password contains illegal characters.")
+            return False, 'FPC'
+
+        if password.isalpha() or password.isdigit() or password.isalnum():
+            print("Password is too easy to guess.")
+            return False, 'FPC'
+
+        table[userID] = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        saveTable(table)
+        print("OK")
+        return True, 'SPC'
+
+    else:
+        print("Invalid Credentials.")
+
+        if table.get(userID, None) != None:
+            return False, 'FPC'
+        else:
+            return False, ''
+    
+def listUsers():
+    """
+    Lists the users 
+    """
+    table = openTable()
+
+    for user in table:
+        print(user)
+
+    print("OK")
+    return None
+
+def openTable():
+    """
+    Opens the databaseFile.txt
+    """
+    with open("loginFile.txt", "rb") as f:
+        table = pickle.load(f)
+        f.close()
+    
+    return table
+
+def saveTable(table):
+    with open("loginFile.txt", "wb") as f:
+        pickle.dump(table,f)
+    
+    f.close()
+
+    return None
